326ad4d311cb834c41cdbdb8648f63781d84b3b6
MohammadGhouse <mohammadghouse016@gmail.com>
2025-06-08 14:51:18 +0530

    Added service annotation

--- src/main/java/com/coding/employee/service/EmployeeService.java
+++ src/main/java/com/coding/employee/service/EmployeeService.java
@@ -1,9 +1,11 @@
 package com.coding.employee.service;
 
 import com.coding.employee.entity.Employee;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public interface EmployeeService {
 
     Employee createEmployee(Employee employee);
