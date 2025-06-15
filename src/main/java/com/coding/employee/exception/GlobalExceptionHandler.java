5e288be6bf19512f63f28cac3897b9fea2a9b7cb
MohammadGhouse <mohammadghouse016@gmail.com>
2025-06-15 14:12:52 +0530

    Method not valid exception handled

--- src/main/java/com/coding/employee/exception/GlobalExceptionHandler.java
+++ src/main/java/com/coding/employee/exception/GlobalExceptionHandler.java
@@ -3,10 +3,13 @@ package com.coding.employee.exception;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -33,6 +36,20 @@ public class GlobalExceptionHandler {
                 request.getRequestURL().toString()
         );
         return new ResponseEntity<>(errorResponse,HttpStatus.NOT_FOUND);
+    }
+@ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex, HttpServletRequest request){
+        Map<String,String> errors= new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(e->errors.put(e.getField(),e.getDefaultMessage()));
+
+        ErrorResponse errorResponse = new ErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.BAD_REQUEST.value(),
+                HttpStatus.BAD_REQUEST.getReasonPhrase(),
+                ex.getMessage(),
+                request.getRequestURL().toString()
+        );
+    return new ResponseEntity<>(errorResponse,HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(Exception.class)
