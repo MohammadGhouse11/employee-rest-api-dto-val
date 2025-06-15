a665b7713fbea6f03d71f682597f17959379dc89
MohammadGhouse <mohammadghouse016@gmail.com>
2025-06-15 14:05:59 +0530

    Validation annotations included in dto

--- src/main/java/com/coding/employee/dto/EmployeeDTO.java
+++ src/main/java/com/coding/employee/dto/EmployeeDTO.java
@@ -2,10 +2,17 @@ package com.coding.employee.dto;
 
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 public class EmployeeDTO {
+    @NotBlank(message = "Name cannot be blank")
     private String name;
+    @NotBlank(message = "Email cannot be blank")
+    @Pattern(regexp = "^[A-Za-z0-9+_%.-]+@[A-Za-z0-9.-]$",
+            message = "Email must be in valid format(e.g:user@example.com)")
     private String email;
+    @NotBlank(message = "Department cannot be blank")
     private String department;
     private String designation;
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
